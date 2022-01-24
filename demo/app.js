21afef4664b096a999db08bc567de3be17be8608
duongtdn <duongtdn@gmail.com>
2022-01-24 18:07:47 +0700

    add App.jsx

--- demo/app.js
+++ demo/app.js
@@ -1,32 +1,8 @@
 "use strict"
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { render } from "react-dom";
 
-import DrawingBoard from "../src/DrawingBoard";
-
-const App = () => {
-
-  const [pen, setPen] = useState("pencil");
-
-  return (
-    <div className = "container">
-      <div> ISOMETRIC </div>
-      <div>
-        <button onClick = {saveImage}>Save</button>
-        <button onClick = {e => setPen("pencil")}>Draw</button>
-        <button onClick = {e => setPen("eraser")}>Eraser</button>
-      </div>
-      <DrawingBoard width = "1200px" height = "700px" pen = {pen} />
-    </div>
-  );
-
-  function saveImage() {
-    // const canvas = board.current;
-    // const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-    // window.location.href=image;
-  }
-
-};
+import App from "../src/App";
 
 render(<App />, document.getElementById("root"));
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+"use strict"
+
+import React, { useState } from "react";
+
+import DrawingBoard from "./DrawingBoard";
+
+export default function() {
+
+  const [pen, setPen] = useState("pencil");
+
+  return (
+    <div className = "container">
+      <h3> ISOMETRIC </h3>
+      <div className="w3-bar w3-border w3-border-gray w3-round" style = {{margin: "8px 0"}}>
+        <button className="w3-button w3-bar-item" onClick = {e => setPen("pencil")}>Draw</button>
+        <button className="w3-button w3-bar-item" onClick = {e => setPen("eraser")}>Eraser</button>
+      </div>
+      <DrawingBoard width = "1200px" height = "700px" pen = {pen} />
+    </div>
+  );
+
+}
