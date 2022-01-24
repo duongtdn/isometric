fecc70ade24a18b048cf8a3286089d439e551fa3
duongtdn <duongtdn@gmail.com>
2022-01-24 19:22:55 +0700

    decorate the header a bit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ export default function() {
 
   return (
     <div className = "container">
-      <h3> ISOMETRIC </h3>
+      <h3 className="w3-text-blue">
+        ISOMETRIC <label className="w3-text-black">|</label> <span className="w3-small w3-text-gray">Free to play</span>
+        <label className="w3-small w3-text-blue-gray w3-right" style={{textAlign: "right"}}>
+          Design by Duong Nguyen <br/>
+          <a href = "https://github.com/duongtdn"> <i className="fab fa-github-alt"></i> Source code </a>
+        </label>
+      </h3>
       <div className="w3-bar w3-border w3-border-gray w3-round" style = {{margin: "8px 0"}}>
         <button className="w3-button w3-bar-item" onClick = {e => setPen("pencil")}>
           <i className="fas fa-pencil-alt"></i> Pencil
