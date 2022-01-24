3b2134b2da4881bf1c25687a11ad49b5d80bc08b
duongtdn <duongtdn@gmail.com>
2022-01-24 19:14:52 +0700

    add icons into memnu buttons

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,12 @@ export default function() {
     <div className = "container">
       <h3> ISOMETRIC </h3>
       <div className="w3-bar w3-border w3-border-gray w3-round" style = {{margin: "8px 0"}}>
-        <button className="w3-button w3-bar-item" onClick = {e => setPen("pencil")}>Draw</button>
-        <button className="w3-button w3-bar-item" onClick = {e => setPen("eraser")}>Eraser</button>
+        <button className="w3-button w3-bar-item" onClick = {e => setPen("pencil")}>
+          <i className="fas fa-pencil-alt"></i> Pencil
+        </button>
+        <button className="w3-button w3-bar-item" onClick = {e => setPen("eraser")}>
+          <i className="fas fa-eraser"></i> Eraser
+        </button>
       </div>
       <DrawingBoard width = "1200px" height = "700px" pen = {pen} />
     </div>
