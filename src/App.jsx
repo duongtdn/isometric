69ad018f5708ad778b6cc3e9f45f94298934b51a
duongtdn <duongtdn@gmail.com>
2022-01-24 22:46:49 +0700

    update the path to source code

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function() {
         ISOMETRIC <label className="w3-text-black">|</label> <span className="w3-small w3-text-gray">Free to play</span>
         <label className="w3-small w3-text-blue-gray w3-right" style={{textAlign: "right"}}>
           Design by Duong Nguyen <br/>
-          <a href = "https://github.com/duongtdn"> <i className="fab fa-github-alt"></i> Source code </a>
+          <a href = "https://github.com/duongtdn/isometric"> <i className="fab fa-github-alt"></i> Source code </a>
         </label>
       </h3>
       <div className="w3-bar w3-border w3-border-gray w3-round" style = {{margin: "8px 0"}}>
